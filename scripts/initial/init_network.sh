7128f7c7ffbcb8e21ee6df19072aa566b6f46bbf
liuxg <liuxg@aone.ai>
2018-08-17 15:05:08 +0800

    mdp-initialise

---
+++ scripts/initial/init_network.sh
@@ -0,0 +1,6 @@
+#! /bin/bash -e
+netdev=`ls /sys/class/net/`
+
+if grep eth0 netdev >> /dev/null; then
+
+fi
---
+++ scripts/initial/ssh_withoutpw.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -e
+if [ $# -ne 2 ] || [ $1 == '-h' ] || [ $1 == '--help' ]; then
+echo -e "\E[35mUsage: $0 hostip password\E[0m"
+exit
+fi
+
+SSHPASS=$2
+export SSHPASS
+
+if [ grep -v $1 /etc/hosts >> /dev/null ]; then 
+echo -e "$1\tdn1" >> /etc/hosts
+fi
+
+if [ ! -f /root/.ssh/id_rsa ]; then
+ssh-keygen -t rsa -P '' -f /root/.ssh/id_rsa 
+fi
+sshpass -e ssh-copy-id root@$1
